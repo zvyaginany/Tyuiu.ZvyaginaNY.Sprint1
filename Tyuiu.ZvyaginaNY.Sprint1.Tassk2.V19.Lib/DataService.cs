c87e122ba92fc4a8d725621d7bfba20b3dfb4845
Tyuiu.NzvyaginaNY@outlook.com <nadiazvagina455@gmail.com>
2025-09-26 19:13:10 +0500

    Task2.V19

---
+++ Tyuiu.ZvyaginaNY.Sprint1.Tassk2.V19.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ZvyaginaNY.Sprint1.Tassk2.V19.Lib
+{
+    public class DataService : ISprint1Task2V19
+    {
+        public double ConvertInchToKm(int value)
+        {
+            return value * value;
+        }
+    }
+}
---
+++ Tyuiu.ZvyaginaNY.Sprint1.Tassk2.V19/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.ZvyaginaNY.Sprint1.Tassk2.V19.Lib;
+
+namespace Tyuiu.ZvyaginaNY.Sprint1.Tassk2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #0 | Выполнила: Звягина Н. Ю. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                       *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #19                                                            *");
+            Console.WriteLine("* Выполнила: Звягина Надежда Юрьевна. | ИСПб-25-1                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.            *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            
+
+            Console.Write("Введите расстояние в дюймах: ");
+            string? s = Console.ReadLine();
+            int inches = int.Parse(s);
+
+            const double inchesToMeters = 0.0254;
+
+            double meters = inches * inchesToMeters;
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine($"Расстояние в метрах: {meters:F3}");
+        }
+    }
+
+
+
+}
+   
