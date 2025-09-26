8a24c97bcf855f8d5642600af45234525d115e50
Tyuiu.NzvyaginaNY@outlook.com <nadiazvagina455@gmail.com>
2025-09-26 19:33:09 +0500

    Task2.V19

--- Tyuiu.ZvyaginaNY.Sprint1.Task2.V19.Test/DataServiceTest1.cs
+++ Tyuiu.ZvyaginaNY.Sprint1.Task2.V19.Test/DataServiceTest1.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.ZvyaginaNY.Sprint1.Task2.V19.Test
             DataService ds = new DataService();
             int x = 1;
             var res = ds.ConvertInchToKm(x);
-            Assert.AreEqual(0, 0508, res);
+            Assert.AreEqual(0.254, res);
         }
     }
 }
