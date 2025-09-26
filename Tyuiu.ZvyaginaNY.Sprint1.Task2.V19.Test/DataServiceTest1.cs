72e00333729c44431264b7296f8406a5b948512f
Tyuiu.NzvyaginaNY@outlook.com <nadiazvagina455@gmail.com>
2025-09-26 19:27:07 +0500

    Task2.V19

---
+++ Tyuiu.ZvyaginaNY.Sprint1.Task2.V19.Test/DataServiceTest1.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZvyaginaNY.Sprint1.Task2.V19.Lib;
+
+namespace Tyuiu.ZvyaginaNY.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(0, 0508, res);
+        }
+    }
+}
