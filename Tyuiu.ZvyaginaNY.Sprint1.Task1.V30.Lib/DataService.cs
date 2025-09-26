1e0b5d1b631dcc46c5f8f9a75d46b0442520bfd0
Tyuiu.NzvyaginaNY@outlook.com <nadiazvagina455@gmail.com>
2025-09-26 17:10:23 +0500

    Task1.V30

---
+++ Tyuiu.ZvyaginaNY.Sprint1.Task1.V30.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ZvyaginaNY.Sprint1.Task1.V30.Lib
+{
+    public class DataService : ISprint1Task1V30
+    {
+        public double Calculate(double x)
+        {
+            return (2 + x) / 2;
+        }
+    }
+}
---
+++ Tyuiu.ZvyaginaNY.Sprint1.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZvyaginaNY.Sprint1.Task1.V30.Lib;
+
+namespace Tyuiu.ZvyaginaNY.Sprint1.Task1.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1.5, res);
+        }
+    }
+}
