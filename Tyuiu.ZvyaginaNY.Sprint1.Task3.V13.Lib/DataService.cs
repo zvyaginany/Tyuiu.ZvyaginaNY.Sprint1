79aff4defd5a960b66be23e585c65b94b29dab04
Tyuiu.NzvyaginaNY@outlook.com <nadiazvagina455@gmail.com>
2025-09-27 19:27:27 +0500

    Task3.V13

--- Tyuiu.ZvyaginaNY.Sprint1.Task3.V13.Lib/DataService.cs
+++ Tyuiu.ZvyaginaNY.Sprint1.Task3.V13.Lib/DataService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.ZvyaginaNY.Sprint1.Task3.V13.Lib
 {
-    public class DataService
+    public class DataService: ISprint1Task3V13
     {
         public double MultiplyOfDigits(double number)
         {
